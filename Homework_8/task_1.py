9d8b73d9a3d7e9340c265ce8b3bbd370f795c182
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-07-11 13:36:32 +0300

    task1 completed

--- Homework_8/task_1.py
+++ Homework_8/task_1.py
@@ -14,23 +14,32 @@ ks = np.array([401, 574, 874, 919, 459, 739, 653, 902, 746, 832])
 
 cov = np.mean(zp*ks) - np.mean(zp) * np.mean(ks) #Ковариация в ручную
 
-print(cov)
-print(np.cov(zp,ks,ddof= 0)) #Смещенная ковариация
+print(f'Ковариация в ручную: {cov}')
+print(f'Ковариация смещенная: {np.cov(zp,ks,ddof= 0)}') #Смещенная ковариация
 
 pirson = cov / np.std(zp, ddof= 0) * np.std(ks, ddof= 0) #коэффециент Пирсона
-print(pirson)
+print(f'Коэффециент Пирсона: {pirson}')
 
-print(np.corrcoef(zp,ks)) # Линейная зависимость есть, и она сильная
+print(f'Линейная зависимость и сила зависимости: {np.corrcoef(zp,ks)}') # Линейная зависимость есть, и она сильная
 
 x = np.mean(zp)
 t_x = stats.t.ppf(0.95,9)
 sigma_x = np.std(zp,ddof=0)
 n_x = np.sqrt(len(zp))
-print((x - t_x) * (sigma_x / n_x), (x + t_x) * (sigma_x / n_x))
+print(f' Доверительный интервал у среднего значения заработной платы: {(x - t_x) * (sigma_x / n_x), (x + t_x) * (sigma_x / n_x)}')
 
 y = np.mean(ks)
 t_y = stats.t.ppf(0.95,9)
 sigma_y = np.std(ks,ddof=0)
 n_y = np.sqrt(len(ks))
-print((y - t_y) * (sigma_y / n_y), (y + t_y) * (sigma_y / n_y))
+print(f' Доверительный интервал у среднего значения кредитного скоринга: {(y - t_y) * (sigma_y / n_y), (y + t_y) * (sigma_y / n_y)}')
+
+plt.scatter(zp, ks)
+plt.title('Experiment')
+plt.xlabel('wages')
+plt.ylabel('credit scoring')
+plt.tight_layout()
+plt.show()
+#  по графику можно сделать вывод, что чем больше заработная плата, тем больше значение кредитного скоринга
+
 
--- Seminary/Seminar_8.py
+++ Seminary/Seminar_8.py
@@ -1,6 +1,6 @@
-# import scipy.stats as stats
-# import numpy as np
-# import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+import matplotlib.pyplot as plt
 
 # x = np.array([10,8, 13, 9,11,14, 6,4,12, 7,5])
 # y = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68 ])
@@ -14,9 +14,14 @@
 # различия между средними 2х нормально распределенных генеральных совокупностей,
 # представленных следующими независимыми выборками:
 
-# a = np.array([12, 10, 11, 19, 13, 11, 17, 15, 19, 14, 21, 18, 21, 11, 17, 14, 15, 17, 20, 19])
-# b = np.array([11, 13, 18, 15, 17, 18, 10, 21, 26, 15, 11, 12, 15, 17, 10, 18, 18, 12,21, 20])
+a = np.array([12, 10, 11, 19, 13, 11, 17, 15, 19, 14, 21, 18, 21, 11, 17, 14, 15, 17, 20, 19])
+b = np.array([11, 13, 18, 15, 17, 18, 10, 21, 26, 15, 11, 12, 15, 17, 10, 18, 18, 12,21, 20])
 
+plt.scatter(a, b)
+plt.title('Experiment 1')
+# coef1, pval1 = stats.pearsonr(x1, y1)
+plt.xlabel('x')
+plt.ylabel('y')
 # plt.scatter(a,b)
 # plt.xlabel('x')
 # plt.ylabel('x')
@@ -45,32 +50,32 @@
 
 # print((P_AB * P_B)/P_A)
 
-import scipy.stats as stats
-import numpy as np
-import matplotlib.pyplot as plt
+# import scipy.stats as stats
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-x1 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
-y1 = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68])
+# x1 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+# y1 = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68])
 
-x2 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
-y2 = np.array([9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74])
+# x2 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+# y2 = np.array([9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74])
 
-x3 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
-y3 = np.array([7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73])
+# x3 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+# y3 = np.array([7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73])
 
-x4 = np.array([8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8])
-y4 = np.array([6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89])
+# x4 = np.array([8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8])
+# y4 = np.array([6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89])
 
-x5 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5, 15, 16, 18])
-y5 = np.array([9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74, 6.5, 5, 2.9])
+# x5 = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5, 15, 16, 18])
+# y5 = np.array([9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74, 6.5, 5, 2.9])
 
-plt.subplot(231)
-plt.scatter(x1, y1)
-plt.title('Experiment 1')
-coef1, pval1 = stats.pearsonr(x1, y1)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.text(4, 11, f'r={coef1:.2f}')
+# # plt.subplot(231)
+# plt.scatter(x1, y1)
+# plt.title('Experiment 1')
+# # coef1, pval1 = stats.pearsonr(x1, y1)
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.text(4, 11, f'r={coef1:.2f}')
 
 # plt.subplot(232)
 # plt.scatter(x2, y2)
