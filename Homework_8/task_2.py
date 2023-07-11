6a8ce5130f13d2baebe07c66903e380efb516678
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-07-11 13:47:08 +0300

    task_2 completed

--- Homework_8/task_2.py
+++ Homework_8/task_2.py
@@ -3,6 +3,14 @@
 # Найдите доверительный интервал для математического ожидания с надежностью 0.95.
 
 import numpy as np
+from scipy import stats
 
 
 x = np.array([131, 125, 115, 122, 131, 115, 107, 99, 125, 111])
+
+M_x = np.mean(x)
+sigma = np.std(x,ddof=1)
+n = len(x)
+t = stats.t.ppf(0.95, 9)
+
+print(f'Доверительный интервал = {(M_x - t) * (sigma / np.sqrt(n)) , (M_x + t) * (sigma / np.sqrt(n))} ')
