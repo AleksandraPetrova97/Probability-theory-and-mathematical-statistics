af4fbfb1bb13cd49a547dcd415219e854d367bbf
unknown <mal.alek.97@gmail.com>
2023-07-21 13:35:29 +0300

    ok

--- neparam_test.py
+++ neparam_test.py
@@ -1,16 +1,16 @@
-import numpy as np
-import scipy.stats as stats
+# import numpy as np
+# import scipy.stats as stats
 
-x1 = np.array([47,90,75,74.5,85,93])
-x2 = np.array([58,60,77,67,73.5,89])
+# x1 = np.array([47,90,75,74.5,85,93])
+# x2 = np.array([58,60,77,67,73.5,89])
 
-# Тест Манна- Уитни в Python
-print(stats.mannwhitneyu(x1,x2))
+# # Тест Манна- Уитни в Python
+# print(stats.mannwhitneyu(x1,x2))
 
-# Критерий Уилкоксона 
+# # Критерий Уилкоксона 
 
-print(x2 - x1)
-print(stats.wilcoxon(x1,x2))
-
+# print(x2 - x1)
+# print(stats.wilcoxon(x1,x2))
+print('a')
 
 
